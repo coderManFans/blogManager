125d973242cae585cf0c88a410aacba9f21217a1
fanchunshuai <2217011466@qq.com>
2014-12-30 19:59:16 +0800

    为blogContent，blogType设置get/set方法， 2014年12月30日19:58:23

--- src/main/java/cn/com/blog/model/BlogContent.java
+++ src/main/java/cn/com/blog/model/BlogContent.java
@@ -13,4 +13,51 @@ public class BlogContent {
     private String blogContent;                 //博客内容
     private String blogIntro;                   //简介
 
+    public int getBlogid() {
+        return blogid;
+    }
+
+    public void setBlogid(int blogid) {
+        this.blogid = blogid;
+    }
+
+    public List<BlogType> getBlogTypeList() {
+        return blogTypeList;
+    }
+
+    public void setBlogTypeList(List<BlogType> blogTypeList) {
+        this.blogTypeList = blogTypeList;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getBlogTitle() {
+        return blogTitle;
+    }
+
+    public void setBlogTitle(String blogTitle) {
+        this.blogTitle = blogTitle;
+    }
+
+    public String getBlogContent() {
+        return blogContent;
+    }
+
+    public void setBlogContent(String blogContent) {
+        this.blogContent = blogContent;
+    }
+
+    public String getBlogIntro() {
+        return blogIntro;
+    }
+
+    public void setBlogIntro(String blogIntro) {
+        this.blogIntro = blogIntro;
+    }
 }
--- src/main/java/cn/com/blog/model/BlogType.java
+++ src/main/java/cn/com/blog/model/BlogType.java
@@ -4,4 +4,31 @@ package cn.com.blog.model;
  * Created by fcs on 14-12-27.
  */
 public class BlogType {
+    private  int blogTypeId;
+    private  String blogTypeName;
+    private  String blogTag;
+
+    public int getBlogTypeId() {
+        return blogTypeId;
+    }
+
+    public void setBlogTypeId(int blogTypeId) {
+        this.blogTypeId = blogTypeId;
+    }
+
+    public String getBlogTypeName() {
+        return blogTypeName;
+    }
+
+    public void setBlogTypeName(String blogTypeName) {
+        this.blogTypeName = blogTypeName;
+    }
+
+    public String getBlogTag() {
+        return blogTag;
+    }
+
+    public void setBlogTag(String blogTag) {
+        this.blogTag = blogTag;
+    }
 }
